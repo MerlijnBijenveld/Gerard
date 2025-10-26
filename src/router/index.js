bc29c5720294d91932fc54d429cb59b894bd74f8
Merlijn Bijenveld <merlijn.bijenveld@hva.nl>
2025-10-26 16:36:30 +0100

    chore: test

--- src/router/index.js
+++ src/router/index.js
@@ -1,41 +1,44 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/App.vue'
-import AboutView from '../views/AboutPage.vue'
-import MediaView from '../views/MediaPage.vue'
-import ContactPage from '@/views/ContactPage.vue'
-import ArtworkShowcase from '@/views/ArtworkShowcase.vue'
+import {
+  createRouter,
+  createWebHistory,
+} from "https://unpkg.com/vue-router@4/dist/vue-router.esm-browser.js";
+import HomeView from "../views/App.vue";
+import AboutView from "../views/AboutPage.vue";
+import MediaView from "../views/MediaPage.vue";
+import ContactPage from "@/views/ContactPage.vue";
+import ArtworkShowcase from "@/views/ArtworkShowcase.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
+    path: "/",
+    name: "home",
     component: HomeView,
   },
   {
-    path: '/about',
-    name: 'about',
+    path: "/about",
+    name: "about",
     component: AboutView,
   },
   {
-    path: '/media',
-    name: 'media',
+    path: "/media",
+    name: "media",
     component: MediaView,
   },
   {
-    path: '/contact',
-    name: 'contact',
+    path: "/contact",
+    name: "contact",
     component: ContactPage,
   },
   {
-    path: '/artworks',
-    name: 'artworks',
+    path: "/artworks",
+    name: "artworks",
     component: ArtworkShowcase,
   },
-]
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
-})
+});
 
-export default router
+export default router;
