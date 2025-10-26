8e321c0c18afed80e3eaa742733326e3dfe009a5
Merlijn Bijenveld <merlijn.bijenveld@hva.nl>
2025-10-26 16:41:00 +0100

    chore: test

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
-// ...existing code...
 import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
 import App from "./RootComponent.vue";
-import router from "./router";
+import router from "./router/index.js";
 
 const app = createApp(App);
 app.use(router);
