ba3b5f025a76e85b1e6e5bee591778fd3f2e67c5
Merlijn Bijenveld <merlijn.bijenveld@hva.nl>
2025-10-27 09:47:47 +0100

    test

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 // ...existing code...
 
 import { createApp } from 'vue'
-import App from './RootComponent.vue'
-import router from '.'
+import App from './src/RootComponent.vue'
+import router from './src/router'
 
 const app = createApp(App)
 app.use(router)
